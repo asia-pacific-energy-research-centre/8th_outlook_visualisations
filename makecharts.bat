b9bdbfdb01fa843fbf73e338123d79fc4a1ce0d4
David Wogan <david.m.wogan@gmail.com>
2021-04-18 19:05:05 +0900

    add windows batch script

--- makecharts.bat
+++ makecharts.bat
@@ -1,10 +1,15 @@
-call conda activate ./env
-python ./workflow/scripts/1_historical_to_projected/OSeMOSYS_to_EGEDA.py
+python ./workflow/scripts/1_historical_to_projected/OSeMOSYS_to_EGEDA_2018.py
 
-python ./workflow/scripts/2_charts_tables/FED_tfc_economy.py
+python ./workflow/scripts/2_charts_tables/FED_tfc_economy_ref_netz.py
 
-python ./workflow/scripts/2_charts_tables/TPES_economy.py
+python ./workflow/scripts/2_charts_tables/TPES_economy_ref_netz.py
 
-python ./workflow/scripts/2_charts_tables/Transformation_economy.py
+python ./workflow/scripts/2_charts_tables/Transformation_economy_ref_netz.py
 
-python ./workflow/scripts/2_charts_tables/OSeMOSYS_detailed.py
+python ./workflow/scripts/2_charts_tables/OSeMOSYS_detailed_ref_netz.py
+
+python ./workflow/scripts/3_emissions/1_historical_emissions_build.py
+
+python ./workflow/scripts/3_emissions/2_osemosys_to_historical.py
+
+python ./workflow/scripts/3_emissions/3_emissions_charts.py
